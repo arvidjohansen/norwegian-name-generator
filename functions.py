e2fc6499ff84d60885a692abf1f6f299dd9b9d47
Biskus <biskus@gmail.com>
2021-03-31 05:34:35 +0200

    yolo

--- functions.py
+++ functions.py
@@ -17,42 +17,50 @@ fn_both_genders = [n for n in norrøne_fornavn] #98
 fn_all = fn_male + fn_female + fn_both_genders #3033
 surnames = etternavn #1460
 
-
-#fnavn_nor_jente = [n for n in fnavn_begge if n in norrøne_kvinnenavn] #28 ???
-#fnavn_nor_gutt = [n for n in fnavn_begge if n not in norrøne_kvinnenavn] #70 ???
-
-class Name():
-    def __init__(self, firstname, surname):
+class Person():
+    def __init__(self, firstname, surname, gender):
         self.fname = firstname
         self.sname = surname
+        self.gender = gender # 'm' or 'f' or '?'
     
-    username_policies = [
-        'fullfname', #full first name + first char last
-        '3first3last', #3 first from fname, 3 first from last
-    ]
-
-    def un_policy_fullfname(fname,lname):
-        return f'{fname[:3]}{lname[-3:]}'
-
-    def un_policy_3first3last(fname,lname):
-        return f'{fname}{lname[0]}'
+    @property
+    def username(self, ):
+        fname = self.fname.replace(' ','') #remove spaces
+        return (fname + self.sname[0]).lower() #full fname + 1st in lastname
+        #return (fname[:3].replace(' ','') + self.sname[:3]).lower() #3 first in fname + 3 first in sname
     
+    @property
+    def name(self):
+        return f'{self.fname} {self.sname}'
 
-    def username(self, policy='fullfname'):
-        return getattr(self,policy(self.fname,self.sname))
+    def __repr__(self):
+        return f'({self.gender}): {self.fname} {self.sname} -> {self.username} ({len(self.username)})'
+
+def rand_person():
+    fname = random.choice(fn_all)
+    surname = random.choice(surnames)
+    if fname in fn_male: gender = 'm'
+    elif fname in fn_female: gender = 'f'
+    else: gender = '?'
+
+    return Person(fname, surname, gender=gender)
+
+def n_rand_people(n=100):
+    return [rand_person() for n in range(n)]
+
+def export_csv():
+    with open('names.csv','w') as f:
+        f.write(f'gender,username,firstname,surname\n')
+        for p in n_rand_people():
+            f.write(f'{p.gender},{p.username},{p.fname},{p.sname}\n')
+
+def main():
+    export_csv()
+
+if __name__ == '__main__':
+    main()
 
 
 
-def rand_fullname():
-    return f'{random.choice(fn_all)} {random.choice(surnames)}'
 
-
-def n_rand_fullnames(n=10):
-    names = []
-    for i in range(n):
-        pick = rand_fullname()
-        names.append(pick)
-    return names
-
-print(n_rand_fullnames())
 
